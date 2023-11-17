bafdcd1544bfe461c4423eb14ff34f26b9d03353
124476 <124476@bk.ru>
2023-11-17 20:50:58 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import sqlite3
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        con = sqlite3.connect("../git_project1/coffee.sqlite")
+        cur = con.cursor()
+        res = cur.execute("SELECT * FROM Coffi").fetchall()
+        self.tableWidget.setColumnCount(len(res[0]))
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
+        self.tableWidget.setRowCount(0)
+        for row, items in enumerate(res):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for col, item in enumerate(items):
+                self.tableWidget.setItem(row, col, QTableWidgetItem(str(item)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
